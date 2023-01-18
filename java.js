ed099734bbc9665b5ed3c916cc7e68f05d535149
name0-Sansnom <67804626+name0-Sansnom@users.noreply.github.com>
2023-01-18 12:09:20 +0000

    Navbar and its responsive mode with slider update

--- java.js
+++ java.js
@@ -1,10 +1,31 @@
+// Autoplay Slider
 var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
   pagination: {
     el: ".swiper-pagination",
-    type: "progressbar",
+    clickable: true,
   },
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
 });
+
+// responsive Header
+
+$(document).ready(function () {
+  $(".menu-trigger").click(function () {
+    $("ul li").slideToggle(500);
+  }); //end slide toggle
+
+  $(window).resize(function () {
+    if ($(window).width() > 500) {
+      $("ul li").removeAttr("style");
+    }
+  }); //end resize
+}); //end ready
