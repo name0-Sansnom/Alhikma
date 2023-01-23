e8d579af12ac108cf8aef28aca9609b65e788d3e
Mansur <93473981+MuhammedMasuru@users.noreply.github.com>
2023-01-23 09:30:49 +0000

    Add files via upload

---
+++ java.js
@@ -0,0 +1,13 @@
+// responsive Header
+
+$(document).ready(function () {
+  $(".menu-trigger").click(function () {
+    $("ul li").slideToggle(500);
+  }); //end slide toggle
+
+  $(window).resize(function () {
+    if ($(window).width() > 500) {
+      $("ul li").removeAttr("style");
+    }
+  }); //end resize
+}); //end ready
