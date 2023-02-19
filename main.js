735aa62d22d0a22dbee250a53bf84ca16223493b
name0-Sansnom <67804626+name0-Sansnom@users.noreply.github.com>
2023-02-19 11:31:57 +0000

    load more button

--- main.js
+++ main.js
@@ -4,31 +4,31 @@ lightbox.option({
   disableScrolling: true,
 });
 
-// let loadMoreBtn = document.querySelector("#load-more");
-// let loadLessBtn = document.querySelector("#load-less");
-// let currentItem = 4;
+let loadMoreBtn = document.querySelector("#load-more");
+let loadLessBtn = document.querySelector("#load-less");
+let currentItem = 4;
 
-// loadMoreBtn.onclick = () => {
-//   let boxes = [...document.querySelectorAll(".container .box-container .box")];
-//   for (var i = currentItem; i < currentItem + 4; i++) {
-//     boxes[i].style.display = "inline-block";
-//   }
-//   currentItem += 4;
+loadMoreBtn.onclick = () => {
+  let boxes = [...document.querySelectorAll(".container .box-container .box")];
+  for (var i = currentItem; i < currentItem + 4; i++) {
+    boxes[i].style.display = "inline-block";
+  }
+  currentItem += 4;
 
-//   if (currentItem >= boxes.length) {
-//     loadMoreBtn.style.display = "none";
-//     loadLessBtn.style.display = "inline-block";
-//   }
-// };
+  if (currentItem >= boxes.length) {
+    loadMoreBtn.style.display = "none";
+    loadLessBtn.style.display = "inline-block";
+  }
+};
 
-// loadLessBtn.onclick = () => {
-//   let boxes = [...document.querySelectorAll(".container .box-container .box")];
-//   for (var i = currentItem; i > currentItem + 4; i--) {
-//     boxes[i].style.display = "inline-block";
-//   }
-//   currentItem -= 4;
+loadLessBtn.onclick = () => {
+  let boxes = [...document.querySelectorAll(".container .box-container .box")];
+  for (var i = currentItem; i > currentItem + 4; i--) {
+    boxes[i].style.display = "inline-block";
+  }
+  currentItem -= 4;
 
-//   if (currentItem <= boxes.length) {
-//     loadLessBtn.style.display = "inline-block";
-//   }
-// };
+  if (currentItem <= boxes.length) {
+    loadLessBtn.style.display = "inline-block";
+  }
+};
